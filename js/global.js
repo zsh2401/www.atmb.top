6f32fe7e13049a9ad69e2f03e11377fe1c8353d1
zsh2401 <zsh2401@163.com>
2018-12-19 21:45:01 +0800

    推送2019预览版1,增加全站公告系统

--- js/global.js
+++ js/global.js
@@ -6,12 +6,32 @@ depend on
 -Valine
 -JQuery
 */
+var __websiteNoticeUrl = "/_data_/notice.json";
+var __websiteNoticeCtrlId = "website-notice";
 console.log("本网站仓库:https://github.com/zsh2401/www.atmb.top/ 欢迎贡献代码!");
 function fetchJson(url,successCallBack){
   fetch(url)
     .then(res=>res.json())
     .then(json=>{
       successCallBack(json);
+    }).catch(e=>{
+        console.log("can't get " + url + " " + e);
+    })
+}
+function initWebsiteNotice(){
+    fetch(__websiteNoticeUrl)
+    .then(res=>res.json())
+    .then(notice=>{
+        var noticeCtrl =  document.getElementById(__websiteNoticeCtrlId);
+        if(notice.enable){
+            noticeCtrl.style.visibility="visible";
+            for(var i = 0 ;i<notice.classes.length;i++){
+                noticeCtrl.classList.add(notice.classes[i]);
+            }
+            noticeCtrl.innerHTML = notice.content;
+        }else{
+            noticeCtrl.style.visibility="hidden";
+        }
     }).catch(e=>{
         console.log("can't get " + url + " " + e);
     })
@@ -39,3 +59,6 @@ function getUrlParam(name){
     var r = window.location.search.substr(1).match(reg);  //匹配目标参数
     if (r != null) return unescape(r[2]); return null; //返回参数值
 }
+$(document).ready(()=>{
+    initWebsiteNotice();
+});
