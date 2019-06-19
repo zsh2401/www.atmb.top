6afc0e02f64406779334e8b737135c969a37b0b6
zsh2401 <zsh2401@163.com>
2019-06-20 00:07:45 +0800

    完成donate页面的jade化

--- jade/render.js
+++ jade/render.js
@@ -1,10 +1,13 @@
-const config = require("./config");
 const fs = require("fs");
 const jade = require("jade");
+const pageData = JSON.parse(fs.readFileSync("./data/pageconst.json"));
+const config = JSON.parse(fs.readFileSync("./data/config.json"));
+pageData.donate = handleDonateData();
+
 function renderAll(){
-    for(let i = 0;i<config.files.length;i++){
-        let crt = config.files[i];
-        jade.renderFile(crt.jade,config.data,(error,html)=>{
+    for(let i = 0;i<config.jades.length;i++){
+        let crt = config.jades[i];
+        jade.renderFile(crt.filename,pageData,(error,html)=>{
             if(error != null){
                 console.log(error);
             }else{
@@ -12,5 +15,34 @@ function renderAll(){
             }
         });
     }
+}
+function handleDonateData(){
+    let donateData = {};
+    donateData.donors = JSON.parse(fs.readFileSync("./data/donors.json"));
+    donateData.donateTotalAmount = 0;
+    donateData.donateTimes = 0;
+    for(let i =0;i<donateData.donors.length;i++){
+        donateData.donateTotalAmount += donateData.donors[i].amount;
+        donateData.donateTimes ++;
+    }
+    donateData.donors = donateData.donors.sort(donateDataSort);
+    donateData.donateTotalAmount = donateData.donateTotalAmount.toFixed(2);
+    return donateData;
+}
+function donateDataSort(a, b) {
+    if (a.amount === b.amount ) {
+        return isEarlyThan(a,b);
+    } else {
+        return b.amount - a.amount;
+    } 
+}
+
+function isEarlyThan(a,b){
+    const __d = new Date();
+    var c0 = a.date.split('/');
+    var c1 = b.date.split('/');
+    var d0 = __d.setFullYear(c0[2], c0[0], c0[1]);
+    var d1 = __d.setFullYear(c1[2], c1[0], c1[1]);
+    return d1 - d0;
 }
 renderAll();
