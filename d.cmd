1c20f197ff5469630109c715287ddc3f34dee58c
zsh2401 <zsh2401@163.com>
2019-08-18 18:18:01 +0800

    修改部署cmd

--- d.cmd
+++ d.cmd
@@ -1,6 +1,9 @@
 @ECHO OFF
+echo 'building...'
+npm run build
+echo 'deploying...'
 cd dist
-echo 'removing old git files'
+echo 'removing old git files...'
 rd/s/q .git
 echo 'reinit git env'
 git init
