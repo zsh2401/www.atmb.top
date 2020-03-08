3c22241fd21e3339c62e9a10720b719a79a7dbba
zsh2401 <zsh2401@163.com>
2020-03-08 21:42:57 +0800

    更新deploy.js

--- deploy.js
+++ deploy.js
@@ -1,6 +1,9 @@
 const ghpages = require('gh-pages')
 const path = require('path');
-ghpages.publish(path.resolve(__dirname,'./docs/.vuepress/dist'),{
-    branch:"master",
-    repo:"git@e.coding.net:studio2401/www.atmb.top/deploy.git"
-});
+(() => {
+    ghpages.publish(path.resolve(__dirname, './docs/.vuepress/dist'), {
+        branch: "master",
+        repo: "git@e.coding.net:studio2401/www.atmb.top/deploy.git"
+    });
+})();
+
