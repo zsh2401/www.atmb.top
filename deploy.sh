3238e98d6c1185dd56f30ec1ec97b515ca419314
zsh2401 <zsh2401@163.com>
2020-08-02 19:01:35 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd ./docs/.vuepress/dist
 git init
+git config user.email "zsh2401@163.com"
+git config user.name "zsh2401"
 git add .
 git commit -m "Updates."
 git push --force $REPO master:master        
