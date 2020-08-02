6f52d5b746c9c07395c61870e777f5f947bf0172
zsh2401 <zsh2401@163.com>
2020-08-02 18:38:29 +0800

    修改deploy.js

--- deploy.js
+++ deploy.js
@@ -2,12 +2,17 @@
 const ghpages = require('gh-pages')
 const path = require('path');
 if(!process.env.TOKEN){
-
     throw "Token not defined in ENV!!!!!";
 }
 (() => {
+    const repo = "https://" + process.env.TOKEN + "@e.coding.net/studio2401/www.atmb.top/deploy.git";
+    // console.log(repo);
     ghpages.publish(path.resolve(__dirname, './docs/.vuepress/dist'), {
         branch: "master",
-        repo: "https://" + process.env.TOKEN + "@e.coding.net/studio2401/www.atmb.top/deploy.git"
+        repo
+    },(err)=>{
+        if(err){
+            throw err;
+        }
     });
 })();
