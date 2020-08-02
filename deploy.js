0cb59357a4ba77ee635bb5d9f04f0fb6a2f5d01b
zsh2401 <zsh2401@163.com>
2020-08-02 18:04:00 +0800

    更新deploy脚本行为

--- deploy.js
+++ deploy.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
 const ghpages = require('gh-pages')
 const path = require('path');
 (() => {
     ghpages.publish(path.resolve(__dirname, './docs/.vuepress/dist'), {
         branch: "master",
-        repo: process.env.TOKEN + "@e.coding.net:studio2401/www.atmb.top/deploy.git"
+        repo: "https://" + process.env.TOKEN + "@e.coding.net:studio2401/www.atmb.top/deploy.git"
     });
 })();
