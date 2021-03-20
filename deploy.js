b947d1bd5d8b2a5da96b3cdd43c87c80225f1838
zsh2401 <zsh2401@163.com>
2021-03-20 13:26:19 +0800

    ?

--- deploy.js
+++ deploy.js
@@ -12,7 +12,7 @@ if (!process.env.FULL_REPO) {
     throw "FULL_REPO is not defined in ENV!!!!!. It's should be like this: https://username:password@xxx.xxx.git";
 }
 
-console.log(FULL_REPO)
+console.log(process.env.FULL_REPO)
 ghpages.publish(path.resolve(__dirname, "./docs/.vuepress/dist"), {
     repo: process.env.FULL_REPO
 }, (err) => {
