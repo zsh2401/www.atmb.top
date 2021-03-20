ece7aac39b0f2f537d1f71fd956ab30b790048ec
zsh2401 <zsh2401@163.com>
2021-03-20 13:01:30 +0800

    use full repo

--- deploy.js
+++ deploy.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
+/*
+ * Created on Sat Mar 20 2021
+ *
+ * Version 2
+ * Copyright (c) 2021 Seymour Zhang
+ */
+
 const ghpages = require('gh-pages')
 const path = require('path');
-if (!process.env.USERNAME) {
-    throw "USERNAME is not defined in ENV!!!!!";
+if (!process.env.FULL_REPO) {
+    throw "FULL_REPO is not defined in ENV!!!!!. It's should be like this: https://username:password@xxx.xxx.git";
 }
-if (!process.env.PASSWORD) {
-    throw "PASSWORD is not defined in ENV!!!!!";
-}
-(() => {
-    const repo = `https://${USERNAME}:${PASSWORD}@e.coding.net/studio2401/www.atmb.top/deploy.git`;
-    ghpages.publish(path.resolve(__dirname, './docs/.vuepress/dist'), {
-        branch: "master",
-        repo
-    }, (err) => {
-        if (err) {
-            throw err;
-        }
-    });
-})();
+
+ghpages.publish(path.resolve(__dirname, "./docs/.vuepress/dist"), {
+    repo: FULL_REPO
+}, (err) => {
+    if (err) {
+        throw err;
+    }
+});
