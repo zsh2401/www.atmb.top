c0a8104a118551b2e4c6f582b88a381ccc345d93
zsh2401 <zsh2401@163.com>
2021-03-20 12:19:25 +0800

    use github to deploy pages

--- deploy.js
+++ deploy.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 const ghpages = require('gh-pages')
 const path = require('path');
-if(!process.env.TOKEN){
-    throw "Token is not defined in ENV!!!!!";
+if (!process.env.USERNAME) {
+    throw "USERNAME is not defined in ENV!!!!!";
+}
+if (!process.env.PASSWORD) {
+    throw "PASSWORD is not defined in ENV!!!!!";
 }
 (() => {
-    const repo = "https://" + process.env.TOKEN + "@e.coding.net/studio2401/www.atmb.top/deploy.git";
-    // console.log(repo);
+    const repo = `https://${USERNAME}:${PASSWORD}@e.coding.net/studio2401/www.atmb.top/deploy.git`;
     ghpages.publish(path.resolve(__dirname, './docs/.vuepress/dist'), {
         branch: "master",
         repo
-    },(err)=>{
-        if(err){
+    }, (err) => {
+        if (err) {
             throw err;
         }
     });
