2ef51590f7a71603bbf2636ddf3bbb0d3aef31c2
zsh2401 <zsh2401@163.com>
2021-03-24 20:44:19 +0800

    no longer deploy to other repos

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ./docs/.vuepress/dist
-git init
-git config user.email "zsh2401@163.com"
-git config user.name "zsh2401"
-git add .
-git commit -m "Updates."
-git push --force $REPO master:master        
